f46cb8de5fb94da3e96e1cd69425ee5125a00b7c
Anurag Dhungana <anuragdhungana1@gmail.com>
2020-01-31 11:15:27 +0545

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:climator/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -30,7 +32,18 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getData() async {
     Response response = await get(
         "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
-    print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodeData = jsonDecode(data);
+      var temperature = decodeData['main']['temp'];
+      var condition = decodeData['weather'][0]['id'];
+      var cityName = decodeData['name'];
+      print(temperature);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
@@ -39,8 +52,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (content) => LocationScreen()));
+            getData();
             //Get the current location
           },
           child: Text('Get Location'),
